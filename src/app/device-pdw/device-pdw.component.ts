703c5863f508facb0941ac65fa4680a9a8abe4ff
mzvast <mzvast@gmail.com>
2017-05-21 22:36:48 +0800

    显示修正

--- src/app/device-pdw/device-pdw.component.ts
+++ src/app/device-pdw/device-pdw.component.ts
@@ -9,7 +9,75 @@ import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@
 })
 export class DevicePdwComponent implements OnInit {
 
-  constructor() { }
+  count: number = 50;
+  rows: any[] = [];
+  active: boolean = true;
+  temp: any[] = [];
+  cols: any = [
+    'name', 'gender', 'company'
+  ];
+
+  constructor() {
+    this.fetch((data) => {
+      this.rows = data.map(d => {
+        d.updated = Date.now().toString();
+        return d;
+      });
+
+      this.temp = [...this.rows];
+    });
+
+    this.start();
+  }
+
+  randomNum(start: number, end: number): number {
+    return Math.floor(Math.random() * end) + start;
+  }
+
+  start(): void {
+    if (!this.active) return;
+
+    setTimeout(this.updateRandom.bind(this), 50);
+  }
+
+  stop(): void {
+    this.active = false;
+  }
+
+  add() {
+    this.rows.splice(0, 0, this.temp[this.count++]);
+  }
+
+  remove() {
+    this.rows.splice(0, this.rows.length);
+  }
+
+  updateRandom() {
+    const rowNum = this.randomNum(0, 5);
+    const cellNum = this.randomNum(0, 3);
+    const newRow = this.randomNum(0, 100);
+    const prop = this.cols[cellNum];
+    const rows = this.rows;
+
+    if (rows.length) {
+      const row = rows[rowNum];
+      row[prop] = rows[newRow][prop];
+      row.updated = Date.now().toString();
+    }
+
+    this.start();
+  }
+
+  fetch(cb: any): void {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/company.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
 
   ngOnInit() {
   }
