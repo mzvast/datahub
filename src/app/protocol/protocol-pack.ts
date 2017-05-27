46b17f1dfbb220c8ef5ace5f46b401cb4ac91eec
Terry <yaoxinghuo@qq.com>
2017-05-27 14:03:41 +0800

    初步解析ProtocolPack

--- src/app/protocol/protocol-pack.ts
+++ src/app/protocol/protocol-pack.ts
@@ -1,3 +1,5 @@
+import {Buffer} from "buffer";
+import {serialize} from "@angular/compiler/src/i18n/serializers/xml_helper";
 /**
  * Created by Terry on 2017-5-26.
  */
@@ -6,15 +8,48 @@
  * 通信报文
  */
 export class ProtocolPack {
-  private header = 0x5555; // 数据头 2
-  private end  = 0xAAAA; // 帧结束符 2
-  private len: number; // 数据长度 2
+  // private header = 0x5555; // 数据头 2
+  // private end  = 0xAAAA; // 帧结束符 2
+  // private len: number; // 数据长度 2
   private source: number; // 源地址 2
   private dest: number; // 目的地址 2
-  private idcodePrimary; // 主识别码 2
-  private idcodeSecondly; // 子识别码 2
-  private serial; // 序号 4
-  private frameCount; // 帧包数 4
-  private data; // 数据
-  private checkSum; // 和校验 2
+  private idcodePrimary: number; // 主识别码 2
+  private idcodeSecondly: number; // 子识别码 2
+  private serial: number; // 序号 4
+  private frameCount: number; // 帧包数 4
+  public data; // 数据 Buffer
+  // private checkSum: number; // 和校验 2
+
+  constructor(source: number, dest: number, idcodePrimary: number, idcodeSecondly: number, serial: number, frameCount: number, data) {
+    this.source = source;
+    this.dest = dest;
+    this.idcodePrimary = idcodePrimary;
+    this.idcodeSecondly = idcodeSecondly;
+    this.serial = serial;
+    this.frameCount = frameCount;
+    this.data = data;
+  }
+
+  // 判断参数里的包和本包是否同一个
+  // TODO 还不知道方法是否正确，最好问清楚
+  isTheSamePack(pack: ProtocolPack) {
+    if (pack.serial - this.serial !== 1) {
+      return false;
+    }
+    if (pack.source !== this.source) {
+      return false;
+    }
+    return (this.idcodePrimary === pack.idcodePrimary && this.idcodeSecondly === pack.idcodeSecondly);
+  }
+
+  // 数据包是否完整了
+  isComplete() {
+    return this.frameCount - this.serial === 1;
+  }
+
+  appendData(data: Buffer) {
+    this.data = Buffer.concat([this.data, data]);
+    this.serial++;
+  }
+
 }
--- src/app/udp.service.ts
+++ src/app/udp.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import {Buffer} from 'buffer';
-import { ProtocolPack } from 'app/protocol/protocol-pack';
+import {Injectable} from "@angular/core";
+import {Buffer} from "buffer";
+import {ProtocolPack} from "app/protocol/protocol-pack";
 
 declare var electron: any; // 　Typescript 定义
 
@@ -12,6 +12,9 @@ export class UdpService {
   RemotePORT = 6011;
   LocalHOST = '127.0.0.1';
   RemoteHOST = '127.0.0.1';
+
+  workingBuffer = Buffer.alloc(0);
+  workingProtocolPacks = [];
 
   constructor() {
     console.log('udp service constructor');
@@ -39,7 +42,7 @@ export class UdpService {
     this.server.on('message', (msg, rinfo) => {
       // const buf = Buffer.from(msg);
       console.log(`server got: ${msg.toString('hex')} from ${rinfo.address}:${rinfo.port}`);
-      this.parserProtocolPack(msg);
+      this.parserProtocolPack(Buffer.from(msg));
       console.log(`server got: ${msg.length} bytes`);
       // this.stopUdpServer();
     });
@@ -60,13 +63,59 @@ export class UdpService {
   }
 
   parserProtocolPack(msg: Buffer) {
-    // TODO 先确定msg长度要超过某个值
-    const header: number  = msg.readUInt16BE(0, false);
-    const len: number = msg.readUInt16BE(2, false);
-    console.log(`read pack header: ${header.toString(16)}, length: ${len.toString(16)}`);
-    console.log();
-    // TODO need parser msg, and notify client
-    const protocolPack  = new ProtocolPack();
+    this.workingBuffer = Buffer.concat([this.workingBuffer, msg]);
+    let headerFounded = false;
+    while (this.workingBuffer.length >= 2 && !headerFounded) {
+      const header: number = this.workingBuffer.readUInt16BE(0, false);
+      if (header === 0x5555) {
+        headerFounded = true;
+      } else {
+        // 如果header不是0x5555,就说明不正常，把这个从数据包里删掉，看接下来的2个字节
+        this.workingBuffer = this.workingBuffer.slice(2);
+        console.log(`read pack header not 0x5555, drop it: ${header.toString(16)}`);
+      }
+
+      if (headerFounded) {
+        // 只有超过1024才解析，否则等下一个包来的时候继续
+        if (this.workingBuffer.length >= 1024) {
+          const len: number = this.workingBuffer.readUInt16BE(2, false);
+          const source: number = this.workingBuffer.readUInt16BE(4, false);
+          const dest: number = this.workingBuffer.readUInt16BE(6, false);
+          const idcodePrimary: number = this.workingBuffer.readUInt16BE(8, false);
+          const idcodeSecondly: number = this.workingBuffer.readUInt16BE(10, false);
+          const serial: number = this.workingBuffer.readUInt32BE(12, false);
+          const frameCount: number = this.workingBuffer.readUInt32BE(16, false);
+          const data = this.workingBuffer.slice(20, 20 + len);
+          const checkSum = this.workingBuffer.readUInt16BE(1020, false);
+          const end = this.workingBuffer.readUInt16BE(1022, false);
+          // TODO 要根据checkSum和end来判断这条数据是否正确
+
+          const protocolPack = new ProtocolPack(source, dest, idcodePrimary, idcodeSecondly, serial, frameCount, data);
+          if (frameCount === 1) {
+            // TODO parser and notify the UI and save to sqlite
+          } else {
+            if (serial === 0) {
+              this.workingProtocolPacks.concat(protocolPack);
+            } else {
+              for (const workingProtocolPack of this.workingProtocolPacks) {
+                if (workingProtocolPack.isTheSamePack(protocolPack)) {
+                  workingProtocolPack.appendData(protocolPack.data);
+                  if (workingProtocolPack.isComplete()) {
+                    // TODO parser and notify the UI and save to sqlite
+                    // remove it from workingProtocolPacks
+                  }
+                }
+              }
+            }
+          }
+
+          // 数据1024都用完了,把headerFounded变成false，再次进入循环解析
+          this.workingBuffer = this.workingBuffer.slice(1024);
+          headerFounded = false;
+        }
+
+      }
+    }
   }
 
   setLocalAddress(host: string, port: number) {
