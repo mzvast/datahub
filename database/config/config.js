e050b9e6cbf01e74c96cda0baaab3a806e943209
mzvast <mzvast@gmail.com>
2017-11-14 00:36:25 +0800

    refactor(sequelize)  add connection pool option

--- database/config/config.js
+++ database/config/config.js
@@ -1,14 +1,20 @@
 module.exports = {
-  "development": {
-    "storage": "./database/dev_db.sqlite",
-    "dialect": "sqlite"
-  },
-  "test": {
-    "storage": "./database/test_db.sqlite",
-    "dialect": "sqlite"
-  },
-  "production": {
-    "storage": "./database/prod_db.sqlite",
-    "dialect": "sqlite"
-  }
-}
+    development: {
+        storage: './database/dev_db.sqlite',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        dialect: 'sqlite'
+    },
+    test: {
+        storage: './database/test_db.sqlite',
+        dialect: 'sqlite'
+    },
+    production: {
+        storage: './database/prod_db.sqlite',
+        dialect: 'sqlite'
+    }
+};
